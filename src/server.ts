d43b2d1a9e18082f4fc72a2a5673916d6f2669f9
Jhone0202 <maicon.jhone100@gmail.com>
2021-02-24 11:38:25 -0300

    Aula_02-Create_User

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-// Routes
-app.get('/users', (req, res) => {
-    return res.json({
-        "res": "Imagine uma lista de usuários"
-    });
-});
-
-app.post('/user', (req, res) => {
-    return res.json({
-        "res": "Usuário adicionado com sucesso!"
-    });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'))
